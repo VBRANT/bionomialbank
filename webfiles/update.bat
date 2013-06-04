8e30d9dc985ef1fc04f518396ec03c416587cc7e
Guido Sautter <sautter@ipd.uka.de>
2013-06-04 23:35:52 +0200

    Adjusted build scripts and static folder structure to preserve file timestamps in build output

---
+++ webfiles/update.bat
@@ -0,0 +1,3 @@
+REN BinoBank.war BinoBank.zip
+pkunzip -no BinoBank.zip -x *.cnfg *ooter.html
+REN BinoBank.zip BinoBank.war
---
+++ webfiles/update.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+mv BinoBank.war BinoBank.zip
+unzip -uo BinoBank.zip -x *.cnfg *ooter.html
+mv BinoBank.zip BinoBank.war
